d54e4ce141172dfa65afec3965241ac57e14b796
Brad <admin@stuxnet.xyz>
2015-10-04 16:09:40 +0100

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -1,42 +1,40 @@
 <?php
 
 function smarthomeroute_list(){
-	$sql = "SELECT id, name FROM smarthomeroute";
+	$sql = "SELECT cid FROM smarthomeroute";
 	$results= sql($sql, "getAll");
 
 	foreach($results as $result){
-		$customers[] = array($result[0],$result[1]);
+		$customers[] = array($result[0]);
 	}
 	return isset($customers)?$customers:null;
 }
 
 function smarthomeroute_get($extdisplay){
-	$sql="SELECT * FROM smarthomeroute where id=$extdisplay";
+	$sql="SELECT * FROM smarthomeroute where cid=$extdisplay";
 	$results=sql($sql, "getRow", DB_FETCHMODE_ASSOC);
 	return isset($results)?$results:null;
 }
 
-function smarthomeroute_add($name, $cid, $destination){
-	$sql="INSERT INTO smarthomeroute (name, cid, destination) values ('$name', '$cid', '$destination')";
+function smarthomeroute_add($cid, $destination){
+	$sql="INSERT INTO smarthomeroute (cid, destination) values ('$cid', '$destination')";
 	sql($sql);
 }
 
 function smarthomeroute_del($extdisplay){
-	$sql="DELETE FROM smarthomeroute where id=$extdisplay";
+	$sql="DELETE FROM smarthomeroute where cid=$extdisplay";
 	sql($sql);
 }
 
-function smarthomeroute_edit($extdisplay, $name, $cid, $destination){
-	$sql="UPDATE smarthomeroute set name='$name' where id='$extdisplay'";
+function smarthomeroute_edit($extdisplay, $cid, $destination){
+	$sql="UPDATE smarthomeroute set cid='$cid' where cid='$extdisplay'";
 	sql($sql);
-	$sql="UPDATE smarthomeroute set cid='$cid' where id='$extdisplay'";
-	sql($sql);
-	$sql="UPDATE smarthomeroute set destination='$destination' where id='$extdisplay'";
+	$sql="UPDATE smarthomeroute set destination='$destination' where cid='$extdisplay'";
 	sql($sql);
 }
 
 function smarthomeroute_getcid(){
-	$sql="SELECT DISTINCT id from cid order by id";
+	$sql="SELECT DISTINCT cid order by cid";
 	$results=sql($sql, "getAll");
 	return isset($results)?$results:null;
 }
